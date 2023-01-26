f7d4deadf9aa691f013be4d1d3900e3335024a02
Scott Clark <scottclark11@gmail.com>
2023-01-26 12:53:32 -0500

    Complete challenge 8

--- aes/aes.go
+++ aes/aes.go
@@ -4,6 +4,9 @@ import (
 	"crypto/aes"
 )
 
+// The AES block size in bytes.
+const BlockSize = 16
+
 // DecryptECB decrypts a ciphertext encrypted via AES-128 in ECB mode using
 // the given key.
 func DecryptECB(ciphertext []byte, key []byte) ([]byte, error) {
@@ -19,4 +22,30 @@ func DecryptECB(ciphertext []byte, key []byte) ([]byte, error) {
 	}
 
 	return plaintext, nil
+}
+
+// DetectECBEncryption returns a number in the range [0, 1] indicating the
+// fraction of blocks of the ciphertext that are duplicated. A higher score
+// indicates a higher likelihood that the ciphertext was encrypted with ECB.
+func DetectECBEncryption(ciphertext []byte) float64 {
+	if len(ciphertext) == 0 {
+		panic("aes.DetectECBEncryption16: empty ciphertext")
+	}
+	if len(ciphertext)%BlockSize != 0 {
+		panic("aes.DetectECBEncryption16: ciphertext size not a multiple of 16")
+	}
+
+	n := len(ciphertext) / BlockSize
+	blocks := make(map[[BlockSize]byte]struct{}, n)
+	var count int
+	for i := 0; i < n; i++ {
+		block := [BlockSize]byte(ciphertext[i*BlockSize : (i+1)*BlockSize])
+		if _, ok := blocks[block]; ok {
+			count++
+		} else {
+			blocks[block] = struct{}{}
+		}
+	}
+
+	return float64((count*2*100)/n) / float64(100)
 }
--- cryptopals_test.go
+++ cryptopals_test.go
@@ -227,3 +227,35 @@ Play that funky music
 		t.Errorf("want: '%x', got: '%x'", want, got)
 	}
 }
+
+func TestChallenge8(t *testing.T) {
+	inputFile := "data/8.txt"
+	want := "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	var got string
+	var score float64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		hexstr := scanner.Text()
+		line := hexMustDecodeString(hexstr)
+		s := aes.DetectECBEncryption(line)
+		if s > score {
+			got = hexstr
+			score = s
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	if want != got {
+		t.Errorf("want: '%s', got: '%s'", want, got)
+	}
+}
