beb7591346828aabd113a1d887fff827c98288f9
Scott Clark <scottclark11@gmail.com>
2023-02-28 21:18:56 -0500

    Comment Cryptopals challenge tests

--- cryptopals_test.go
+++ cryptopals_test.go
@@ -27,6 +27,8 @@ func base64MustDecodeString(s string) []byte {
 	return b
 }
 
+// Convert hex to base64
+// See: https://www.cryptopals.com/sets/1/challenges/1
 func TestChallenge1(t *testing.T) {
 	input := hexMustDecodeString("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	want := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
@@ -37,6 +39,8 @@ func TestChallenge1(t *testing.T) {
 	}
 }
 
+// Fixed XOR
+// See: https://www.cryptopals.com/sets/1/challenges/2
 func TestChallenge2(t *testing.T) {
 	inputA := hexMustDecodeString("1c0111001f010100061a024b53535009181c")
 	inputB := hexMustDecodeString("686974207468652062756c6c277320657965")
@@ -50,6 +54,8 @@ func TestChallenge2(t *testing.T) {
 	}
 }
 
+// Single-byte XOR cipher
+// See: https://www.cryptopals.com/sets/1/challenges/3
 func TestChallenge3(t *testing.T) {
 	input := hexMustDecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 	want := "Cooking MC's like a pound of bacon"
@@ -63,6 +69,8 @@ func TestChallenge3(t *testing.T) {
 	}
 }
 
+// Detect single-character XOR
+// See: https://www.cryptopals.com/sets/1/challenges/4
 func TestChallenge4(t *testing.T) {
 	inputFile := "data/4.txt"
 	want := "Now that the party is jumping\n"
@@ -95,6 +103,8 @@ func TestChallenge4(t *testing.T) {
 	}
 }
 
+// Implement repeating-key XOR
+// See: https://www.cryptopals.com/sets/1/challenges/5
 func TestChallenge5(t *testing.T) {
 	input := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
 	key := []byte("ICE")
@@ -108,6 +118,8 @@ func TestChallenge5(t *testing.T) {
 	}
 }
 
+// Break repeating-key XOR
+// See: https://www.cryptopals.com/sets/1/challenges/6
 func TestChallenge6(t *testing.T) {
 	inputFile := "data/6.txt"
 	want := "Terminator X: Bring the noise"
@@ -126,6 +138,8 @@ func TestChallenge6(t *testing.T) {
 	}
 }
 
+// AES in ECB mode
+// See: https://www.cryptopals.com/sets/1/challenges/7
 func TestChallenge7(t *testing.T) {
 	inputFile := "data/7.txt"
 	key := []byte("YELLOW SUBMARINE")
@@ -148,6 +162,8 @@ func TestChallenge7(t *testing.T) {
 	}
 }
 
+// Detect AES in ECB mode
+// See: https://www.cryptopals.com/sets/1/challenges/8
 func TestChallenge8(t *testing.T) {
 	inputFile := "data/8.txt"
 	want := "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
