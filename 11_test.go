20aa82a6882aa93a1ca3436ad5db053a76180bb3
Scott Clark <scottclark11@gmail.com>
2023-03-11 23:53:41 -0500

    Refactor and fix challenge 11 oracle regression

--- 11_test.go
+++ 11_test.go
@@ -11,22 +11,24 @@ import (
 // An ECB/CBC detection oracle
 // See: https://www.cryptopals.com/sets/2/challenges/11
 func TestChallenge11(t *testing.T) {
-	encrypt, want, err := exploitable.NewModeDetectionOracle()
+	var want bool
+	updateWant := func(isECB bool) {
+		want = isECB
+	}
+
+	encrypt, err := exploitable.NewModeDetectionOracle(updateWant)
 	if err != nil {
 		t.Fatalf("creating oracle: %v", err)
 	}
 
-	isECB, err := exploit.IsOracleECBMode(aes.BlockSize, encrypt)
-	if err != nil {
-		t.Fatalf("detecting mode: %v", err)
-	}
+	for i := 0; i < 10; i++ {
+		got, err := exploit.IsOracleECBMode(aes.BlockSize, encrypt)
+		if err != nil {
+			t.Fatalf("detecting mode: %v", err)
+		}
 
-	got := exploitable.ModeECB
-	if !isECB {
-		got = exploitable.ModeCBC
-	}
-
-	if want != got {
-		t.Errorf("want: '%v', got: '%v'", want, got)
+		if want != got {
+			t.Errorf("want: '%v', got: '%v'", want, got)
+		}
 	}
 }
