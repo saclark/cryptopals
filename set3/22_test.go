c17277f12be6e7ed21f67bc93b24b5dd7fb160bf
Scott Clark <scottclark11@gmail.com>
2023-04-07 00:56:31 -0400

    Complete challenge 22

---
+++ set3/22_test.go
@@ -0,0 +1,24 @@
+package set3
+
+import (
+	"testing"
+	"time"
+
+	"github.com/saclark/cryptopals-go/internal/testutil"
+	"github.com/saclark/cryptopals-go/rand"
+)
+
+func TestChallenge22(t *testing.T) {
+	const maxElapsedSecs = 1000
+	want := uint32(time.Now().Unix()) - uint32(testutil.MustRandomInt(maxElapsedSecs+1))
+	prng := rand.NewMT19937(want)
+
+	got, err := RecoverPRNGSeed(prng.Uint32(), maxElapsedSecs)
+	if err != nil {
+		t.Fatalf("recovering PRNG seed: %v", err)
+	}
+
+	if want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
